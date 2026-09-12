f7c739af84b07422cebecf96f1392d18e10703c8
niteshver <niteshv1520@gmail.com>
2026-09-12 11:05:54 +0530

    Fix Railway allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({ plugins: [react(), tailwindcss()] })
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 8080,
+    allowedHosts: [
+      "brilliant-clarity-production-87cc.up.railway.app",
+    ],
+  },
+});
