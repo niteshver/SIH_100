03d8aa432262a3906b507235d5c75599c10ca6ed
niteshver <niteshv1520@gmail.com>
2026-09-12 19:04:49 +0530

    configure custom frontend domain

--- vite.config.ts
+++ vite.config.ts
@@ -7,26 +7,19 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     allowedHosts: [
-      "brilliant-clarity-production-87cc.up.railway.app",
-      "sih100-production-3f7d.up.railway.app",
       "chatlyme.xyz",
       "www.chatlyme.xyz",
+      "brilliant-clarity-production-87cc.up.railway.app",
     ],
-
-    proxy: {
-      "/api": "http://localhost:8000",
-      "/health": "http://localhost:8000",
-    },
   },
 
   preview: {
     host: "0.0.0.0",
     port: 8080,
     allowedHosts: [
-      "brilliant-clarity-production-87cc.up.railway.app",
-      "sih100-production-3f7d.up.railway.app",
       "chatlyme.xyz",
       "www.chatlyme.xyz",
+      "brilliant-clarity-production-87cc.up.railway.app",
     ],
   },
 });
